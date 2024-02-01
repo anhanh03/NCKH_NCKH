1b9c1fbd2fe8d58d615dbaf6fdfff3b7d1605a7a
anhanh03 <97895905+anhanh03@users.noreply.github.com>
2024-02-01 21:04:17 +0700

    update comment code

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,8 @@ use App\Models\User;
 use App\Models\Topic;
 use Illuminate\Http\Request;
 
-
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -26,65 +24,64 @@ use Laravel\Socialite\Facades\Socialite;
 |
 */
 
-// Route::get('/', function () {
-//     return 'Xin chào!';
-// });
-// Dường dẫn sai 
+// Dường dẫn sai
 Route::fallback(function () {
-    return redirect('/'); 
+    return redirect('/');
 });
 
-Route::get("/signinorsignup", [UserController::class,"index"])->name("signinorsignup");
+// đăng ký đăng nhập
+Route::get('/signinorsignup', [UserController::class, 'index'])->name('signinorsignup');
 
-Route::get("/login", [UserController::class,"login"])->name("login");
+Route::get('/login', [UserController::class, 'login'])->name('login');
 
-Route::get("/signup", [UserController::class,"signup"])->name("signup");
+Route::get('/signup', [UserController::class, 'signup'])->name('signup');
 
-Route::get("/logout", [UserController::class,"logout"])->name("logout");
+Route::get('/logout', [UserController::class, 'logout'])->name('logout');
 
 //Hiển thị trang quản lý bài đăng cho người dùng đăng bài
-Route::get("/displayManage", [UserController::class,"manage"])->name("manage");
+Route::get('/displayManage', [UserController::class, 'manage'])->name('manage');
 
-Route::get("/auth/facebook/callback",function(){
-    return "callback login fb";
+// đăng nhập đăng ký FB, Google
+Route::get('/auth/facebook/callback', function () {
+    return 'callback login fb';
 });
 
-Route::get("/auth/facebook",function(){
+Route::get('/auth/facebook', function () {
     return Socialite::driver('facebook')->redirect();
-})->name("fblogin");
+})->name('fblogin');
 
-Route::get("/auth/google",function(){
+Route::get('/auth/google', function () {
     return Socialite::driver('google')->redirect();
-})->name("gglogin");
+})->name('gglogin');
 
-
-Route::get("/auth/facebook/callback",function(){
-    return "callback login gg";
+Route::get('/auth/facebook/callback', function () {
+    return 'callback login gg';
 });
 
-
-Route::get('/home', [HomeController::class,'index'])->name("home");
-
-Route::get('/', [HomeController::class,'index']);
-
-Route::get("/tpost",[PostController::class,'displayTitlePost'])->name("displayTitlePost");
-
-Route::get("/postcontent",[PostController::class,'displayPost'])->name("displayPost");
+// trang chủ
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index']);
+// hiển thị các bài viết trong chủ đề
+Route::get('/tpost', [PostController::class, 'displayTitlePost'])->name('displayTitlePost');
+//bài viết
+Route::get('/postcontent', [PostController::class, 'displayPost'])->name('displayPost');
 
 //Hiển thị form thêm bài đăng
-Route::get("/displayAddPosst",[PostController::class,'displayAddPost'])->name("displayAddPost");
+Route::get('/displayAddPosst', [PostController::class, 'displayAddPost'])->name('displayAddPost');
+//cập nhật hồ sơ người dùng
+Route::get('/updateuser', [HomeController::class, 'displayInfor'])->name('displayInfor');
+// show comment
+Route::get('/comment', [CommentController::class, 'commentshow'])->name('commentshow');
+// đăng coment
+Route::get('/upcomment', [CommentController::class, 'upComment'])->name('upcomment');
 
-Route::get("/updateuser",[HomeController::class,'displayInfor'])->name("displayInfor");
-
-Route::get("/comment",[CommentController::class,'commentshow'])->name('commentshow');
-
-Route::get('/upcomment',[CommentController::class,'upComment'])->name('upcomment');
-
-Route::get('/createdocument',[DocumentsController::class,'showCreate'])->name('showCreateDocument');
+// Tạo rài liệu
+Route::get('/createdocument', [DocumentsController::class, 'showCreate'])->name('showCreateDocument');
 
 //trang tải tài liệu
-Route::get('/ShowDocument',[DocumentsController::class,'showDocument'])->name('showDocument');
+Route::get('/ShowDocument', [DocumentsController::class, 'showDocument'])->name('showDocument');
 
+// gợi ý chủ đề
 Route::get('/topic-suggestions', function (Request $request) {
     $query = $request->input('query');
     $topics = Topic::where('TopicName', 'like', '%' . $query . '%')->pluck('TopicName');
@@ -92,7 +89,6 @@ Route::get('/topic-suggestions', function (Request $request) {
     return response()->json($topics);
 });
 
-
-Route::get('chinh-sach-rieng-tu',function(){
+Route::get('chinh-sach-rieng-tu', function () {
     return '<h1>Chinh sach rieng tu</h1>';
 });
